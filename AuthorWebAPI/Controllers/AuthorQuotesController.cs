4bc19445ca6ecedd6b8ba8a67c8daf7df619f0fb
ompreetigupta <110689124+ompreetigupta@users.noreply.github.com>
2024-08-11 20:00:19 +0530

    Add project files.

---
+++ AuthorWebAPI/Controllers/AuthorQuotesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AuthorWebAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class AuthorQuotesController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public AuthorQuotesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            
+        }
+        [HttpGet("get_tasks")]
+
+        public JsonResult get_tasks()
+        {
+            string query = "select * from AuthorQuotes";
+            DataTable table = new DataTable();
+            string SqlDatasource = _configuration.GetConnectionString("mydata");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(SqlDatasource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query, myCon)) 
+                {
+                myReader=myCommand.ExecuteReader();
+                table.Load(myReader);
+                }
+            }
+            return new JsonResult(table);
+        }
+
+
+    }
+}
